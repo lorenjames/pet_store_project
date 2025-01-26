a556193a13ea3c2fce2bccbfed3fb41e444c1253
Loren Craig-Mohebali <lorenjames@gmail.com>
2025-01-26 15:44:20 -0600

    Week 14 Coding Assignment Submission

--- pet-store/src/main/java/pet/store/entity/Customer.java
+++ pet-store/src/main/java/pet/store/entity/Customer.java
@@ -20,10 +20,11 @@ public class Customer {
 	
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Long employeeId;
+	private Long customerId;
 	
 	private String customerFirstName;
 	private String customerLastName;
+	private String customerEmail;
 	
 	@EqualsAndHashCode.Exclude
 	@ToString.Exclude
